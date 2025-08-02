7f8347efc3bab727ffd05d536e2a4e5a06814fa2
Girish494 <girishpawar172004@gmail.com>
2025-08-02 10:16:46 +0530

    add new file

---
+++ README.md
@@ -0,0 +1,2 @@
+# New Project 
+In this Project i created landing page on which add friend button is present like as instagram and othe social media platform
---
+++ app.js
@@ -0,0 +1,15 @@
+var istatus=document.querySelector("h5");
+var add=document.querySelector("#add");
+var flag=0;
+add.addEventListener("click",function(){
+if(flag==0){
+    istatus.innerHTML="Friends";
+    istatus.style.color="green";
+    flag=1;
+}else{
+    istatus.innerHTML="Stanger";
+    istatus.style.color="red";
+    flag=0;
+}
+
+})
